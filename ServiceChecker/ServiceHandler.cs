75c58e971d480b37a0a7c8085fb3d2644fe86e7c
Pityubak <42932625+Pityubak@users.noreply.github.com>
2018-10-13 10:03:25 +0200

    Project starting

---
+++ ServiceChecker/ServiceHandler.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Linq;
+using System.ServiceProcess;
+
+
+namespace EsetChecker
+{
+    class ServiceHandler
+    {
+        private readonly string serviceName;
+
+        public ServiceHandler(string serviceName)
+        {
+            this.serviceName = serviceName;
+        }
+
+        public string ServiceName => serviceName;
+
+        public bool ServiceExists()
+        {
+            return ServiceController.GetServices().Any(p => p.ServiceName.Equals(ServiceName));
+        }
+        public bool ServiceRunning()
+        {
+            using(ServiceController sc=new ServiceController())
+            {
+                sc.ServiceName = serviceName;
+                if (sc.Status == ServiceControllerStatus.Running)
+                    return true;
+                else
+                    return false;
+            }
+        }
+        public void ServiceStart()
+        {
+            using(ServiceController sc=new ServiceController())
+            {
+                sc.ServiceName = ServiceName;
+                if (sc.Status == ServiceControllerStatus.Stopped)
+                {
+                    try
+                    {
+                        sc.Start();
+                        sc.WaitForStatus(ServiceControllerStatus.Running);
+                    }
+                    catch (Exception op)
+                    {
+                        new SimpleLogger(AppDomain.CurrentDomain.BaseDirectory).Log(op.Message);
+                    }
+                }
+            }
+        }
+        public void ServiceStop()
+        {
+            using (ServiceController sc = new ServiceController())
+            {
+                sc.ServiceName = ServiceName;
+                if (sc.Status == ServiceControllerStatus.Running)
+                {
+                    try
+                    {
+                        sc.Stop();
+                        sc.WaitForStatus(ServiceControllerStatus.Stopped);
+                    }
+                    catch (Exception op)
+                    {
+                        new SimpleLogger(AppDomain.CurrentDomain.BaseDirectory).Log(op.Message);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ ServiceChecker/SimpleLogger.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+
+
+namespace EsetChecker
+{
+    class SimpleLogger
+    {
+        private readonly string savePath;
+
+        public SimpleLogger(string savePath)
+        {
+            this.savePath = savePath;
+        }
+
+        public void Log(string message)
+        {
+            try
+            {
+                using (StreamWriter sw = new StreamWriter(savePath + "\\" + DateTime.Now.ToShortDateString() + ".log"))
+                {
+                    sw.WriteLine(message);
+                }
+            }
+            catch (Exception)
+            {
+                
+            }
+          
+        }
+    }
+}
